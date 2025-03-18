f2434f5f20a58ddf68a1877c42e5bcb545b23687
Gabriel Bizarria <gabrielbizarriadev@gmail.com>
2025-03-17 23:45:56 -0300

    implemented login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import OrderList from "./components/OrderList";
-import OrderStatus from "./types/OrderStatus";
+// import OrderList from "./components/OrderList";
+// import OrderStatus from "./types/OrderStatus";
+
+import Login from "./pages/Login";
 
 function App() {
-  return OrderList({
-    orders: [
-      {
-        orderNumber: "1233290",
-        customerName: "Jéssica da Silva",
-        orderStatus: OrderStatus.Pending,
-        orderDate: "20/01/2022",
-      },
-      {
-        orderNumber: "4563232",
-        customerName: "João da Silva",
-        orderStatus: OrderStatus.Completed,
-        orderDate: "20/01/2022",
-      },
-    ],
-  });
+  // return OrderList({
+  //   orders: [
+  //     {
+  //       orderNumber: "1233290",
+  //       customerName: "Jéssica da Silva",
+  //       orderStatus: OrderStatus.Pending,
+  //       orderDate: "20/01/2022",
+  //     },
+  //     {
+  //       orderNumber: "4563232",
+  //       customerName: "João da Silva",
+  //       orderStatus: OrderStatus.Completed,
+  //       orderDate: "20/01/2022",
+  //     },
+  //   ],
+  // });
+
+  return Login();
 }
 
 export default App;
