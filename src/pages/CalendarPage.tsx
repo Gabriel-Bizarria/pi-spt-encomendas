3112ffdeb77bf9b35e427c66384cfec5e56e2481
Gabriel Bizarria <gabrielbizarriadev@gmail.com>
2025-03-22 00:02:36 -0300

    implemented calendar component

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,7 +1,91 @@
+import FullCalendar from "@fullcalendar/react";
+import timegridPlugin from "@fullcalendar/timegrid";
+import daygridPlugin from "@fullcalendar/daygrid";
+import ptBrLocale from "@fullcalendar/core/locales/pt-br";
+import "../index.css";
+import colors from "tailwindcss/colors";
+import { EventClickArg } from "@fullcalendar/core/index.js";
+import { useState } from "react";
+
 function CalendarPage() {
+  const [todayText, setTodayText] = useState("Data atual");
+
+  const handleDatesSet = (arg: { view: { type: string } }) => {
+    switch (arg.view.type) {
+      case "timeGridDay":
+        setTodayText("Hoje");
+        break;
+      case "timeGridWeek":
+        setTodayText("Semana atual");
+        break;
+      case "dayGridMonth":
+        setTodayText("Mês atual");
+        break;
+      default:
+        setTodayText("Hoje");
+    }
+  };
+
+  const handleEventClick = (clickInfo: EventClickArg) => {
+    alert(`Event: ${clickInfo.event.title}\nStart: ${clickInfo.event.start}`);
+    // You can add more logic here, such as navigating to a detailed page or opening a modal
+  };
+
   return (
-    <div className="flex justify-center">
-      <h1>Calendar Page</h1>
+    <div className="h-full p-9">
+      <FullCalendar
+        plugins={[timegridPlugin, daygridPlugin]}
+        initialView="timeGridDay"
+        height="100%"
+        locale={ptBrLocale}
+        allDaySlot={false}
+        slotMinTime="06:00:00"
+        slotMaxTime="23:59:00"
+        nowIndicator={true}
+        datesSet={handleDatesSet}
+        slotLabelFormat={{
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        }}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "timeGridDay,timeGridWeek,dayGridMonth",
+        }}
+        buttonText={{
+          today: todayText,
+        }}
+        dayHeaderContent={(args) => {
+          const customDayNames = [
+            "Domingo",
+            "Segunda",
+            "Terça",
+            "Quarta",
+            "Quinta",
+            "Sexta",
+            "Sábado",
+          ];
+          const dayName = customDayNames[args.date.getDay()];
+          const dayNumber = args.date.getDate();
+          return `${dayName}, ${dayNumber}`;
+        }}
+        events={[
+          {
+            title: "Pedido - André",
+            start: "2025-03-21T20:00:00",
+            backgroundColor: colors.green[600],
+            borderColor: colors.green[600],
+          },
+          {
+            title: "Pedido - Maria",
+            start: "2025-03-21T22:30:00",
+            backgroundColor: colors.blue[600],
+            borderColor: colors.blue[600],
+          },
+        ]}
+        eventClick={handleEventClick}
+      />
     </div>
   );
 }
